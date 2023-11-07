d8b67c3bdbffa5937b7cd0c9919927aa3f36a27f
Harsh Kedia <hkedia@hey.com>
2023-11-07 18:38:05 +0530

    submit launch wiring

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -7,15 +7,26 @@ async function httpGetPlanets() {
 
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`)
-  const fetchedLaunches =  await response.json()
-  return fetchedLaunches.sort((a,b) => {
+  const fetchedLaunches = await response.json()
+  return fetchedLaunches.sort((a, b) => {
     return a.flightNumber - b.flightNumber
   })
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    })
+  } catch (err) {
+    return {
+      ok: false
+    }
+  }
 }
 
 async function httpAbortLaunch(id) {
