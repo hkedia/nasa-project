54c209dd5ec2c347130c8abfbe60a69e1ce1c58d
Harsh Kedia <hkedia@hey.com>
2025-05-06 18:38:42 +0530

    connect to mongodb via mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,24 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8001;
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+})
+
 async function startServer() {
+  await mongoose.connect(process.env.DATABASE_URL)
+  
   await loadPlanetsData();
 
   server.listen(PORT, () => {
