498fd4a9285eafff436fc422f9b8629236790db1
Harsh Kedia <hkedia@hey.com>
2025-05-07 07:42:04 +0530

    adding and aborting a launch

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const { parse } = require('csv-parse');
 const fs = require('fs');
 const path = require('path');
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
 
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
@@ -18,24 +18,42 @@ function loadPlanetsData() {
         comment: '#',
         columns: true,
       }))
-      .on('data', (data) => {
+      .on('data', async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          savePlanet(data);
         }
       })
       .on('error', (error) => {
         console.error('Error reading file:', error);
         reject(error);
       })
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on('end', async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found!`);
         resolve();
       })
   })
 }
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({}, {
+    '_id': 0,
+    '__v': 0,
+  })
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne({
+      keplerName: planet['kepler_name'],
+    }, {
+      keplerName: planet['kepler_name']
+    }, {
+      upsert: true
+    })
+  } catch (error) {
+    console.error('Could not save planet', error);
+  }
 }
 
 module.exports = {
